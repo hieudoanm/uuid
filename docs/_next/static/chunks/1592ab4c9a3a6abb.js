46a129b92b2e1933612d1d074504bf5873d6cf2b
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-11-16 13:03:32 +0700

    remove football

--- docs/_next/static/chunks/1592ab4c9a3a6abb.js
+++ docs/_next/static/chunks/1592ab4c9a3a6abb.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/8a1c3d0d23e21d70.js",
   "static/chunks/480e3c14df468efa.js",
   "static/chunks/4de572682f60705e.js",
-  "static/chunks/8ae64bbd087bbf6b.css",
-  "static/chunks/turbopack-8a53ca810c07b926.js"
+  "static/chunks/3383de0b8fcc1f22.css",
+  "static/chunks/turbopack-1d4f8eb5d5073963.js"
 ])
